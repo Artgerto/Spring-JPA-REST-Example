c24f4df3c10aacbb0e6a7435c6019549f6302755
artgerto <argerto@gmail.com>
2019-03-27 16:27:25 +0300

    ResourseNotFoundException class added
    ResourseNotFoundException class added

--- src/main/java/com/building/project/controllers/ApartmentPriceStatsController.java
+++ src/main/java/com/building/project/controllers/ApartmentPriceStatsController.java
@@ -1,9 +1,13 @@
 package com.building.project.controllers;
 
+import com.building.project.exception.ResourceNotFoundException;
 import com.building.project.models.ApartmentPriceStats;
 import com.building.project.repository.ApartmentPriceStatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +19,12 @@ public class ApartmentPriceStatsController {
     @Autowired
    ApartmentPriceStatsRepository apartmentPriceStatsRepository;
 
-    @RequestMapping(value = "/apart/prognosis", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    List<ApartmentPriceStats> getStats() {
-        return  apartmentPriceStatsRepository.findByIdApartment((long)9094);
+    @RequestMapping(value = "/apart/prognosis/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    ResponseEntity<?> getStats(@PathVariable Long id) {
+        List<ApartmentPriceStats> apartmentPriceStatsList = apartmentPriceStatsRepository.findByIdApartment(id);
+        if(apartmentPriceStatsList.size() == 0){
+            throw new ResourceNotFoundException(id);
+        }
+        return new ResponseEntity(apartmentPriceStatsList, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/building/project/exception/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package com.building.project.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private Long id;
+
+    public ResourceNotFoundException(Long id) {
+        super(String.format("Element not found with id='%s'", id));
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
